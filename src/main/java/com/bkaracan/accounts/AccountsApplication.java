8ac6aa7d89f3ce10a73c8b1edf88d343be9b3f59
bkaracan <burakkaracan94@gmail.com>
2023-12-04 15:00:34 +0300

    first initial commit

---
+++ src/main/java/com/bkaracan/accounts/AccountsApplication.java
@@ -0,0 +1,13 @@
+package com.bkaracan.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
