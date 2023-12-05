33aa349a1a8104178f3a6bdd254a55acd31e5820
bkaracan.home <burakkaracan94@gmail.com>
2023-12-05 22:56:23 +0300

    unused import removed from AccountController

--- src/main/java/com/bkaracan/accounts/controller/AccountController.java
+++ src/main/java/com/bkaracan/accounts/controller/AccountController.java
@@ -1,7 +1,6 @@
 package com.bkaracan.accounts.controller;
 
 import com.bkaracan.accounts.constant.AccountConstant;
-import com.bkaracan.accounts.dto.AccountDto;
 import com.bkaracan.accounts.dto.CustomerDto;
 import com.bkaracan.accounts.dto.response.ResponseDto;
 import com.bkaracan.accounts.service.AccountService;
@@ -9,13 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
