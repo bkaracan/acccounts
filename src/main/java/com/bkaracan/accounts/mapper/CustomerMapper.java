5ce9f9bc896bbe47bdf019f3abaa0bfb7252d5f2
bkaracan <burakkaracan94@gmail.com>
2023-12-05 11:27:05 +0300

    service and mapper layer created

---
+++ src/main/java/com/bkaracan/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package com.bkaracan.accounts.mapper;
+
+import com.bkaracan.accounts.dto.CustomerDto;
+import com.bkaracan.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto convertToDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customerDto.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer convertToEntity(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
